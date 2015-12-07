66439af528e3d62d92571c189aa48170253ed90d
Pablo Gabriel <pablo.reds@hotmail.com>
2015-12-07 11:38:42 -0300

    Specifying Destination.java

--- src/br/ufrn/realtime/elevator/model/Destination.java
+++ src/br/ufrn/realtime/elevator/model/Destination.java
@@ -2,36 +2,48 @@ package br.ufrn.realtime.elevator.model;
 
 public class Destination {
 
-	private int elevator;
-	private int floor;
-	private boolean up;
+	/*@ spec_public */ private int elevator;
+	/*@ spec_public */ private int floor;
+	/*@ spec_public */ private boolean up;
 
+	/*@ requires elevator > 0 && floor >= 0;
+        ensures getElevator() == elevator;
+        ensures getFloor() == floor;
+        ensures isUp() == up;
+	 */
 	public Destination(int elevator, int floor, boolean up) {
 		this.elevator = elevator;
 		this.floor = floor;
 		this.up = up;
 	}
 
-	public int getElevator() {
+	/*@ pure */ public int getElevator() {
 		return elevator;
 	}
 
+	/*@ requires elevator > 0;
+	    ensures getElevator() == elevator;
+	 */
 	public void setElevator(int elevator) {
 		this.elevator = elevator;
 	}
 
-	public int getFloor() {
+	/*@ pure */ public int getFloor() {
 		return floor;
 	}
 
+	/*@ requires floor >= 0;
+	    ensures getFloor() == floor;
+	 */
 	public void setFloor(int floor) {
 		this.floor = floor;
 	}
 
-	public boolean isUp() {
+	/*@ pure */ public boolean isUp() {
 		return up;
 	}
 
+	//@ ensures isUp() == up;
 	public void setUp(boolean up) {
 		this.up = up;
 	}
